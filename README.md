ad3184feadfeeed531f7edfe72c2460ff8c7d4ee
Matt Lee <matthewbrentlee@g.harvard.edu>
2016-02-10 19:32:39 +0800

    Added css/style.css, images/matt-lee.jpg, added details to README.md and put content into index.php.

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# README for Project 1
+# README for P1: System Setup and Portfolio
 
-This is Matt Lee's README for Project 1.
+## Live URL
+<http://p1.theoneandonlymattlee.com>
+
+## Description
+This project will give you a short introduction to me. I have also created entries for future projects.
+
+## Demo
+The [link to my video](https://www.youtube.com/channel/UCpynttk3PcE3Zvd8d4sNYtg)
+
+## Details for teaching team
+No additional details.
+
+## Outside code
+* Pure CSS: http://purecss.io/
--- index.php
+++ index.php
@@ -2,9 +2,24 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Matt Lee Project 1</title>
+  <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+  <link rel="Stylesheet" href="css/style.css">
 </head>
 <body>
-	<h1>Matt Lee</h1>
+  <h1>Matt Lee</h1>
+  <img src="images/matt-lee.jpg" alt="Matt Lee">
+  <p>I'm an IT Manager at Tianjin International School in Tianjin, China. I'm pursuing my ALM in Information Management Systems from Harvard Extension School, and I would like to improve my programming and web development skills.</p>
+  <p>About the URL: All of the permutations of my first and last name were taken (story of my life), so I chose theoneandonlymattlee.me...as if my name were more unique. Thanks for reading!</p>
+  <h2>Project 2: Password Generator</h2>
+  <p>View: [Link coming soon!]</p>
+  <p>Github: [Link coming soon!]</p>
+  <h2>Project 3: Lorem Ipsum Generator</h2>
+  <p>View: [Link coming soon!]</p>
+  <p>Github: [Link coming soon!]</p>
+  <h2>Project 4: Final Project</h2>
+  <p>View: [Link coming soon!]</p>
+  <p>Github: [Link coming soon!]</p>
 </body>
 </html>
